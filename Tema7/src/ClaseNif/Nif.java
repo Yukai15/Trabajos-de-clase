3bfe7cfc58869e925527a919be0c7e83f3d11721
Yukai15 <yukaiqiu24@gmail.com>
2026-01-15 11:55:49 +0100

    a

--- Tema7/src/ClaseNif/Nif.java
+++ Tema7/src/ClaseNif/Nif.java
@@ -14,11 +14,10 @@ public class Nif {
 		}
 	char calcularletra(){
 		char[]letras= {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
-		int num =(int) (this.numero%23);
+		int num =(int) (this.numero % 23);
 		return this.letra=letras[num];
 	}
 	public void mostrar() {
-		System.out.println(this.numero + "-" + this.letra);
+		System.out.println(String.format("%08d-%s",this.numero, this.letra));
 	}
-
 }
--- Tema7/src/ClaseNif/Principal.java
+++ Tema7/src/ClaseNif/Principal.java
@@ -1,14 +1,37 @@
 package ClaseNif;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Nif miDNI = new Nif(14017386);
-		//miDNI.dni = 14017386L;
+		/*Nif miDNI = new Nif(14017386);
 		Nif miDNI2 = new Nif(14017386);
+		miDNI.calcularletra();
 		miDNI.mostrar();
+		miDNI2.calcularletra();
 		miDNI2.mostrar();
+		*/
+		Scanner sc=new Scanner(System.in);
+		Nif miNumeroNif=new Nif();
+		System.out.println("Pon números para el Dni");
+		miNumeroNif.numero=sc.nextLong();
+		sc.nextLine();
+		System.out.println("Pon la letra del Dni");
+		char Nifletras=sc.nextLine().charAt(0);
+		miNumeroNif.calcularletra();
+		if(miNumeroNif.letra==Nifletras){
+			System.out.println("El número es correcto");
+		}
+		else {
+			System.out.println("El número es incorrecto");
+			System.out.println("La letra corecto sería "+ miNumeroNif.calcularletra());
+			System.out.println("El número correcto del DNI es ");
+			miNumeroNif.mostrar();
+		}
+		
+		sc.close();
 	}
 
 }
