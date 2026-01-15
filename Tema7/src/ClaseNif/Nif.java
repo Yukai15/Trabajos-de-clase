39f412ac4babe4e52e3d55a6694ac8728de18fae
Yukai15 <yukaiqiu24@gmail.com>
2026-01-15 10:23:40 +0100

    Nuevo

---
+++ Tema7/src/ClaseNif/Nif.java
@@ -0,0 +1,24 @@
+package ClaseNif;
+
+public class Nif {
+	long numero;
+	char letra;
+	Nif(){
+		this.numero=0;
+		this.letra=' ';
+		
+	}
+	Nif(int numDni){
+		this.letra=calcularletra();
+		this.numero=numDni;
+		}
+	char calcularletra(){
+		char[]letras= {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
+		int num =(int) (this.numero%23);
+		return this.letra=letras[num];
+	}
+	public void mostrar() {
+		System.out.println(this.numero + "-" + this.letra);
+	}
+
+}
